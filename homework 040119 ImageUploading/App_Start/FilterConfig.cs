457948b928a0113a1b2795ad9f2cff7a9f11a769
Chayala Kaufman <chayalakaufman@gmail.com>
2019-04-01 21:46:39 -0400

    Add project files.

---
+++ homework 040119 ImageUploading/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace homework_040119_ImageUploading
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
